fb50783dde6a2589d227e616f2501ccbf3430736
Gema <gem.anggada@gmail.com>
2020-07-04 00:50:06 +0700

    chore: now replaces search query to mozilla mdn

--- fckw3.js
+++ fckw3.js
@@ -1,8 +1,26 @@
 /*
 Just Hide w3schools results from search query.
 */
+
+const w3 = "w3scho";
+const w3pattern = /(w3schoo)/gi;
+const w3spattern = /(w3schools)/gi;
+
+var currentPath = window.location.href;
+
+if (w3pattern.test(currentPath) || w3spattern.test(currentPath)) {
+  var newPath = currentPath.replace(w3pattern, "__this_one__");
+  var newPath = currentPath.replace(w3spattern, "__this_one__");
+  newPath = newPath.split("__this_one__").join("mozilla+mdn");
+  window.location.href = newPath;
+}
+
 for (let item of document.getElementsByTagName("a")) {
-  if (item.href.includes("w3school")) {
+  if (
+    item.href.includes(w3) ||
+    w3pattern.test(item.href) ||
+    w3spattern.test(item.href)
+  ) {
     item.parentNode.parentNode.hidden = true;
   }
 }
